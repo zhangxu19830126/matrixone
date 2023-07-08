5c80cb8e71b38c9d6b93496c51387232ec09b1ad
zhangxu <zhangxu19830126@gmail.com>
2023-07-08 18:17:12 +0800

    tmp

--- pkg/sql/colexec/lockop/lock_op.go
+++ pkg/sql/colexec/lockop/lock_op.go
@@ -17,6 +17,7 @@ package lockop
 import (
 	"bytes"
 	"context"
+	"encoding/hex"
 	"fmt"
 	"strings"
 
@@ -91,6 +92,11 @@ func Call(
 	if !txnOp.Txn().IsPessimistic() {
 		return false, nil
 	}
+
+	fmt.Printf("%s in lock op, process %p\n", hex.EncodeToString(txnOp.Txn().ID), proc)
+	defer func() {
+		fmt.Printf("%s out lock op, process %p\n", hex.EncodeToString(txnOp.Txn().ID), proc)
+	}()
 
 	if !arg.block {
 		return callNonBlocking(idx, proc, arg)
