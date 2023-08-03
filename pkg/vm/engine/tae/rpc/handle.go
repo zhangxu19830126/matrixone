f0bf33f14900453bdfb09a9436c3e04e66c4e8dd
triump2020 <triump2020@outlook.com>
2023-08-03 11:39:19 +0800

    add log

--- pkg/vm/engine/tae/rpc/handle.go
+++ pkg/vm/engine/tae/rpc/handle.go
@@ -18,6 +18,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/hex"
+	"fmt"
 	"os"
 	"sync"
 	"syscall"
@@ -148,9 +149,17 @@ func (h *Handle) HandleCommit(
 		}
 		common.DoIfInfoEnabled(func() {
 			if time.Since(start) > MAX_ALLOWED_TXN_LATENCY {
-				logutil.Info("Commit with long latency", zap.Duration("duration", time.Since(start)), zap.String("debug", meta.DebugString()))
+				logutil.Info("Commit with long latency",
+					zap.Duration("duration", time.Since(start)),
+					zap.String("debug", meta.DebugString()))
 			}
 		})
+		if time.Since(start) > MAX_ALLOWED_TXN_LATENCY {
+			fmt.Printf("Commit with logng latency, duration:%f, debug:%s.\n",
+				time.Since(start).Seconds(),
+				meta.DebugString())
+
+		}
 	}()
 	var txn txnif.AsyncTxn
 	if ok {
@@ -160,10 +169,16 @@ func (h *Handle) HandleCommit(
 		if err != nil {
 			return
 		}
+		start := time.Now()
 		err = h.handleRequests(ctx, txn, txnCtx)
 		if err != nil {
 			return
 		}
+		if time.Since(start) > MAX_ALLOWED_TXN_LATENCY {
+			fmt.Printf("Handle request with logng latency, duration:%f.\n",
+				time.Since(start).Seconds())
+		}
+
 	}
 	txn, err = h.db.GetTxnByID(meta.GetID())
 	if err != nil {
@@ -178,12 +193,14 @@ func (h *Handle) HandleCommit(
 
 	if moerr.IsMoErrCode(err, moerr.ErrTAENeedRetry) {
 		for {
+			start := time.Now()
 			txn, err = h.db.StartTxnWithStartTSAndSnapshotTS(nil,
 				types.TimestampToTS(meta.GetSnapshotTS()))
 			if err != nil {
 				return
 			}
 			logutil.Infof("retry txn %X with new txn %X", string(meta.GetID()), txn.GetID())
+			fmt.Printf("retry txn %X with new txn %X.\n", string(meta.GetID()), txn.GetID())
 			//Handle precommit-write command for 1PC
 			h.handleRequests(ctx, txn, txnCtx)
 			//if txn is 2PC ,need to set commit timestamp passed by coordinator.
@@ -193,6 +210,11 @@ func (h *Handle) HandleCommit(
 			err = txn.Commit(ctx)
 			cts = txn.GetCommitTS().ToTimestamp()
 			if !moerr.IsMoErrCode(err, moerr.ErrTAENeedRetry) {
+				if time.Since(start) > MAX_ALLOWED_TXN_LATENCY {
+					fmt.Printf("Retry commit with logng latency, duration:%f, debug:%s.\n",
+						time.Since(start).Seconds(),
+						meta.DebugString())
+				}
 				break
 			}
 		}
@@ -498,6 +520,14 @@ func (h *Handle) EvaluateTxnRequest(
 	ctx context.Context,
 	meta txn.TxnMeta,
 ) (err error) {
+	start := time.Now()
+	defer func() {
+		if time.Since(start) > MAX_ALLOWED_TXN_LATENCY {
+			fmt.Printf("Evaluate request with long latency, duration:%f, debug:%s.\n",
+				time.Since(start).Seconds(),
+				meta.DebugString())
+		}
+	}()
 	h.mu.RLock()
 	txnCtx := h.mu.txnCtxs[string(meta.GetID())]
 	h.mu.RUnlock()
