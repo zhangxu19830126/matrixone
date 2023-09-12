652ec00eb6d34c763db7ecd0613e3165746cff27
zhangxu <zhangxu19830126@gmail.com>
2023-09-12 20:22:12 +0800

    2

--- pkg/cnservice/server.go
+++ pkg/cnservice/server.go
@@ -545,7 +545,7 @@ func (s *service) getTxnClient() (c client.TxnClient, err error) {
 				client.WithEnableRefreshExpression())
 		}
 		opts = append(opts, client.WithEnableLeakCheck(
-			time.Second*70,
+			time.Minute*3,
 			func(txnID []byte, createAt time.Time, createBy string) {
 				runtime.DefaultRuntime().Logger().Fatal("found leak txn",
 					zap.String("txn-id", hex.EncodeToString(txnID)),
