2f568951e34c641907599e95a2800532bc0f50d7
zhangxu <zhangxu19830126@gmail.com>
2023-10-30 18:01:45 +0800

    Revert "fix"
    This reverts commit 99ea711236eb6f416f29e17761b66bc777d475f6.

--- pkg/tnservice/store_rpc_handler.go
+++ pkg/tnservice/store_rpc_handler.go
@@ -16,9 +16,8 @@ package tnservice
 
 import (
 	"context"
-	"time"
-
 	"github.com/matrixorigin/matrixone/pkg/util/trace"
+	"time"
 
 	"github.com/matrixorigin/matrixone/pkg/common/moerr"
 	"github.com/matrixorigin/matrixone/pkg/pb/metadata"
@@ -113,8 +112,8 @@ func (s *store) handleCommit(ctx context.Context, request *txn.TxnRequest, respo
 	if request.CommitRequest != nil {
 		for _, req := range request.CommitRequest.Payload {
 			//response is shared by all requests
-			prepareResponse(&req, response)
-			err := s.handleWrite(ctx, &req, response)
+			prepareResponse(req, response)
+			err := s.handleWrite(ctx, req, response)
 			if err != nil {
 				return err
 			}
